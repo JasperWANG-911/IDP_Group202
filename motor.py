1d64c28e8b58e46b95c67fe36f7b0b27bb580b33
Ruolinnnnn <rl745@cam.ac.uk>
2025-02-25 10:36:23 +0000

    motor test updated

--- motor.py
+++ motor.py
@@ -1,15 +1,33 @@
 from machine import Pin, PWM
-class Motor:
+from utime import sleep
+
+class Motor1:
     def __init__(self):
-        self.m1Dir = Pin(7 , Pin.OUT) # set pin left wheel
-        self.pwm1 = PWM(Pin(6))
-        self.pwm1.freq(1000)
+        self.m1Dir = Pin(7 , Pin.OUT) # set motor direction
+        self.pwm1 = PWM(Pin(6)) # set speed
+        self.pwm1.freq(1000) # set max frequency 
+        self.pwm1.duty_u16(0) # set duty cycle
+    def off(self):
         self.pwm1.duty_u16(0)
-        def off(self):
-            self.pwm1.duty_u16(0)
-        def Forward(self):
-            self.m1Dir.value(0) # forward = 0 reverse = 1 motor 1
+    def Forward(self):
+        self.m1Dir.value(0) # forward = 0 reverse = 1 motor 1
         self.pwm1.duty_u16(int(65535*100/100)) # speed range 0-100 motor 1
-        def Reverse(self):
-            self.m1Dir.value(1)
-            self.pwm1.duty_u16(int(65535*30/100))
+    def Reverse(self):
+        self.m1Dir.value(1)
+        self.pwm1.duty_u16(int(65535*30/100))
+        
+class Motor2:
+    def __init__(self):
+        self.m1Dir = Pin(4 , Pin.OUT) # set motor direction
+        self.pwm1 = PWM(Pin(5)) # set speed
+        self.pwm1.freq(1000) # set max frequency 
+        self.pwm1.duty_u16(0) # set duty cycle
+    def off(self):
+        self.pwm1.duty_u16(0)
+    def Forward(self):
+        self.m1Dir.value(0) # forward = 0 reverse = 1 motor 1
+        self.pwm1.duty_u16(int(65535*100/100)) # speed range 0-100 motor 1
+    def Reverse(self):
+        self.m1Dir.value(1)
+        self.pwm1.duty_u16(int(65535*30/100))
+###
--- motor_test.py
+++ motor_test.py
@@ -1,5 +1,5 @@
 import time
-from motor import Motor  # Import the Motor class from motor.py
+from motor import Motor1, Motor2  # Import the modified motor classes
 
 # ---------------- MotorPair Class ----------------
 class MotorPair:
@@ -7,44 +7,44 @@ class MotorPair:
         self.left = left_motor
         self.right = right_motor
 
-    def move_forward(self, speed=60, duration=1.0):
+    def move_forward(self, duration=1.0):
         """
         Drive both motors forward.
-        Speed is a percentage (0-100). Duration is in seconds.
+        The Motor1 and Motor2 classes internally set the forward speed.
         """
-        self.left.forward(speed)
-        self.right.forward(speed)
+        self.left.Forward()
+        self.right.Forward()
         time.sleep(duration)
         self.left.off()
         self.right.off()
 
-    def move_backward(self, speed=60, duration=1.0):
+    def move_backward(self, duration=1.0):
         """
         Drive both motors in reverse.
         """
-        self.left.reverse(speed)
-        self.right.reverse(speed)
+        self.left.Reverse()
+        self.right.Reverse()
         time.sleep(duration)
         self.left.off()
         self.right.off()
 
-    def turn_left(self, speed=60, duration=1.0):
+    def turn_left(self, duration=1.0):
         """
         Turn in place to the left by stopping the left motor
         and driving the right motor forward.
         """
         self.left.off()
-        self.right.forward(speed)
+        self.right.Forward()
         time.sleep(duration)
         self.left.off()
         self.right.off()
 
-    def turn_right(self, speed=60, duration=1.0):
+    def turn_right(self, duration=1.0):
         """
         Turn in place to the right by driving the left motor forward
         and stopping the right motor.
         """
-        self.left.forward(speed)
+        self.left.Forward()
         self.right.off()
         time.sleep(duration)
         self.left.off()
@@ -52,28 +52,26 @@ class MotorPair:
 
 # ---------------- Main Motor Test ----------------
 if __name__ == "__main__":
-    # Instantiate motors.
-    # For integration we assume:
-    #   - Left motor: direction controlled by pin 3, PWM on pin 2.
-    #   - Right motor: direction controlled by pin 5, PWM on pin 4.
-    left_motor = Motor(dir_pin=3, pwm_pin=2)
-    right_motor = Motor(dir_pin=5, pwm_pin=4)
+    # Instantiate the motors.
+    # Motor1 is used for the left motor and Motor2 for the right motor.
+    left_motor = Motor1()
+    right_motor = Motor2()
     motors = MotorPair(left_motor, right_motor)
 
     print("Testing motor forward")
-    motors.move_forward(speed=60, duration=1.0)
+    motors.move_forward(duration=1.0)
     time.sleep(1)
 
     print("Testing motor reverse")
-    motors.move_backward(speed=60, duration=1.0)
+    motors.move_backward(duration=1.0)
     time.sleep(1)
 
     print("Testing motor turn left")
-    motors.turn_left(speed=60, duration=1.0)
+    motors.turn_left(duration=1.0)
     time.sleep(1)
 
     print("Testing motor turn right")
-    motors.turn_right(speed=60, duration=1.0)
+    motors.turn_right(duration=1.0)
     time.sleep(1)
 
     print("Motor test complete")
