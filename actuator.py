f4b8e2295f2d78e094249c0cb0cbc96a9d6ae56e
JasperWANG-911 <me@wangyinghao.com>
2025-02-27 11:00:13 +0000

    Actuator(DONE)

--- actuator.py
+++ actuator.py
@@ -1,31 +1,27 @@
-from machine import Pin
-import time
+from machine import Pin, PWM
+from utime import sleep
 
-# Define the control pins for extending and retracting
-extend_pin = Pin(15, Pin.OUT)   # Change 15 to your extend control pin number
-retract_pin = Pin(14, Pin.OUT)  # Change 14 to your retract control pin number
-
-def extend_actuator(duration):
-    print("Extending actuator for", duration, "seconds.")
-    extend_pin.value(1)    # Activate extension
-    retract_pin.value(0)   # Ensure retraction is off
-    time.sleep(duration)
-    stop_actuator()
-
-def retract_actuator(duration):
-    print("Retracting actuator for", duration, "seconds.")
-    extend_pin.value(0)    # Ensure extension is off
-    retract_pin.value(1)   # Activate retraction
-    time.sleep(duration)
-    stop_actuator()
-
-def stop_actuator():
-    print("Stopping actuator.")
-    extend_pin.value(0)
-    retract_pin.value(0)
-    time.sleep(1)  # Pause briefly before the next action
-
-# Main loop to continuously extend and retract the actuator
+# Define actuator class
+class Actuator:
+    def __init__(self):
+        self.m1Dir = Pin(0 , Pin.OUT) # set motor direction
+        self.pwm1 = PWM(Pin(1)) # set speed
+        self.pwm1.freq(1000) # set max frequency 
+        self.pwm1.duty_u16(0) # set duty cycle
+    def off(self):
+        self.pwm1.duty_u16(0)
+    def Forward(self):
+        self.m1Dir.value(0) # forward = 0 reverse = 1 motor 1
+        self.pwm1.duty_u16(int(65535*100/100)) # speed range 0-100 motor 1
+    def Reverse(self):
+        self.m1Dir.value(1)
+        self.pwm1.duty_u16(int(65535*30/100))
+        
+# Testing actuator(can be deleted later)
+actuator=Actuator()
 while True:
-    extend_actuator(3)   # Extend for 3 seconds
-    retract_actuator(3)  # Retract for 3 seconds
+     actuator.Forward()
+     sleep(5)
+     actuator.Reverse()
+     sleep(5)
+
