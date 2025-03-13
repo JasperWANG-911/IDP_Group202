67e624c089d6c5fa078f6baee86398b4085aeaad
JasperWANG-911 <me@wangyinghao.com>
2025-03-13 10:13:58 +0000

    actuator updated

--- collection_dropoff.py
+++ collection_dropoff.py
@@ -3,25 +3,6 @@ from utime import sleep
 import hardware_documentation.TOF_sensor
 from hardware_documentation.tcs34725 import html_rgb
 
-# Define actuator class
-class Actuator:
-    def __init__(self):
-        self.m1Dir = Pin(0, Pin.OUT)  # set motor direction
-        self.pwm1 = PWM(Pin(1))       # set speed
-        self.pwm1.freq(1000)          # set max frequency 
-        self.pwm1.duty_u16(0)         # set duty cycle
-
-    def off(self):
-        self.pwm1.duty_u16(0)
-
-    def Forward(self):
-        self.m1Dir.value(0)  # forward = 0, reverse = 1 for motor 1
-        self.pwm1.duty_u16(int(65535 * 100 / 100))  # speed range 0-100 for motor 1
-
-    def Reverse(self):
-        self.m1Dir.value(1)
-        self.pwm1.duty_u16(int(65535 * 30 / 100))
-
 def collection(motor_pair, actuator, TOF_sensor, colour_sensor) -> str:
     """
     Checks box distance.
